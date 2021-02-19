9ee241afb76e5b0bb48b4f5994e4fe66e0c7adcd
ImHwaryeon <ghkfus1603@naver.com>
2021-02-19 12:50:36 +0900

    ..

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,9 +2,8 @@
 	header.header-wrapper
 		.logo
 			i.fa.fa-sun
+			router-link(to='/') HOME
 		ul.navi-wrapper
-			li.navi
-				router-link(to='/') HOME
 			li.navi
 				router-link(to='/daily') DAILY
 			li.navi
@@ -12,9 +11,28 @@
 </template>
 <script>
 export default {
-	
+	name: 'Header',
 }
 </script>
 <style lang="scss" scoped>
-	
+@import '@/assets/scss/_mixins';
+
+.header-wrapper {
+	color: $colorLighter;
+	background-color: $colorDarker;
+	@include flex($SB, $CT);
+	.logo {
+		@include flex($FS, $CT);
+		padding: 1em;
+		i {
+			margin-right: 1em;
+		}
+	}
+	.navi-wrapper {
+		@include flex($FS, $CT);
+		.navi {
+			padding: 1em;
+		}
+	}
+}
 </style>
