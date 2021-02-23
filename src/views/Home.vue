d4f385d1792d002b2e87a079153387c6a221e1a9
ImHwaryeon <ghkfus1603@naver.com>
2021-02-23 12:21:30 +0900

    ..

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,41 @@
 <template lang="pug">
-  .home-wrapper 
-    .title-wrapper
-      h1 Vue를 활용한 날씨정보 앱
-        small.ml-3 vl.0
-    Search
+	.home-wrapper
+		.title-wrapper
+			h2 Vue를 활용한 날씨정보 앱
+				small.ml-3 v1.0
+		Search
+		Daily(:value="GET_DAILY")
 </template>
 <script>
+import { mapGetters } from 'vuex'
 import Search from '../components/Search.vue'
+import Daily from '../components/weather/Daily.vue'
 
 export default {
-  name: 'Home',
-  components : {Search}
+	name: 'Home',
+	components: { Search, Daily },
+	computed: { ...mapGetters(['GET_DAILY']) },
+	created() {
+		this.$store.dispatch('ACT_WEATHER', null )
+		console.log(process.env)
+	},
+	data() {
+		return {
+		
+		}
+	},
+	methods: {
+		
+	}
 }
 </script>
 <style lang="scss" scoped>
-  .home-wrapper{
-    text-align: center;
-  }
+.home-wrapper {
+	text-align: center;
+	@include flex($CT, $CT);
+	flex-direction: column;
+	.Search{
+		
+	}
+}
 </style>
